44c223dd12d7954ab882395e8785d06ff4530e73
Jayreg3 <jeregistre@gmail.com>
2020-03-14 17:28:59 +0100

    Anadi un side drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 import { Ionicons } from "@expo/vector-icons";
 import colors from "../constants/colors";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import HeaderButton from "../components/HeaderButton";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 
 const defaultStackNavOptions = {
   headerStyle: {
@@ -21,12 +25,29 @@ const defaultStackNavOptions = {
   headerTintColor: Platform.OS === "android" ? "white" : colors.primaryColor
 };
 
+const sideDrawer = navData => {
+  return (
+    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      <Item
+        title="Menú"
+        iconName="ios-menu"
+        onPress={() => {
+          navData.navigation.toggleDrawer();
+        }}
+      />
+    </HeaderButtons>
+  );
+};
+
 const MealsNavigator = createStackNavigator(
   {
     Categorías: {
       screen: CategoriesScreen,
-      navigationOptions: {
-        headerTitle: "Categorías de comidas"
+      navigationOptions: navData => {
+        return {
+          headerTitle: "Categorías de comidas",
+          headerLeft: sideDrawer(navData)
+        };
       }
     },
     CategoríasComidas: CategoryMealsScreen,
@@ -39,7 +60,12 @@ const MealsNavigator = createStackNavigator(
 
 const FavNavigator = createStackNavigator(
   {
-    Favorites: FavoritesScreen,
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: navData => {
+        return { headerLeft: sideDrawer(navData) };
+      }
+    },
     MealDetail: MealDetailsScreen
   },
   {
@@ -82,4 +108,20 @@ const MealsFavTabNavigator =
         tabBarOptions: { activeTintColor: colors.accentColor }
       });
 
-export default createAppContainer(MealsFavTabNavigator);
+const FiltersNavigator = createStackNavigator({
+  Filters: {
+    screen: FiltersScreen,
+    navigationOptions: navData => {
+      return {
+        headerLeft: sideDrawer(navData)
+      };
+    }
+  }
+});
+
+const MainNavigator = createDrawerNavigator({
+  MealsFav: MealsFavTabNavigator,
+  Filters: FiltersNavigator
+});
+
+export default createAppContainer(MainNavigator);
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -9,6 +9,10 @@ const FiltersScreen = props => {
   );
 };
 
+FiltersScreen.navigationOptions = {
+  headerTitle: "Filtrar comidas"
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
